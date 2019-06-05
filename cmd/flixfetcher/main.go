fc629c2ea3c59a57ed14d4dc19ad45151ef9a173
zhaolong <zhaolong.liu@flixbus.com>
2019-06-05 18:36:23 +0200

    reformat and restructure code

--- cmd/flixfetcher/main.go
+++ cmd/flixfetcher/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
-	"os"
-	"strconv"
-	"regexp"
 	"io/ioutil"
-	"encoding/json"
-	"time"
-	"errors"
-
-	"github.com/swarvanusg/go_jolokia"
+	"os"
 
 	"github.com/DataDog/kafka-kit/kafkazk"
 
@@ -22,14 +17,14 @@ import (
 // Config holds
 // config parameters.
 type Config struct {
-	Client          *dd.Client
-	APIKey          string
-	AppKey          string
-	ZKAddr          string
-	OutputFilePrefix      string
-	JolokiaPort     int
-	Verbose         bool
-	DryRun          bool
+	Client          	*dd.Client
+	APIKey          	string
+	AppKey          	string
+	ZKAddr          	string
+	OutputFilePrefix    string
+	JolokiaPort     	int
+	Verbose         	bool
+	DryRun          	bool
 }
 
 var config = &Config{} // :(
@@ -45,57 +40,6 @@ func init() {
 
 	envy.Parse("FLIXFETCHER")
 	flag.Parse()
-}
-
-func partitionMetrics(brokerMeta kafkazk.BrokerMetaMap) (map[string]map[string]map[string]float64) {
-	d := map[string]map[string]map[string]float64{}
-	//"kafka.log:name=Size,partition=97,topic=fxt-security-audit-logs,type=Log"
-	r_partition := regexp.MustCompile(`.*partition=(?P<partition>\d+)`)
-	r_topic := regexp.MustCompile(`.*topic=(?P<topic>[^,]+)`)
-	//r_match := regexp.MustCompile(`(?:.*?(?:(.*topic=(?P<topic>[^,]+))|(.*partition=(?P<partition>\d+)))){2}`)
-	for bid := range brokerMeta {
-		m := brokerMeta[bid]
-		jcli := go_jolokia.NewJolokiaClient("http://" + m.Host + ":" + strconv.Itoa(config.JolokiaPort) + "/jolokia/")
-		//curl -XGET "http://127.0.0.1:8778/jolokia/?p=/read/kafka.log:type=Log,name=Size,*"
-		resp, err := jcli.GetAttr("kafka.log:type=Log,name=Size,*", nil, "")
-		if err != nil {
-			fmt.Println(err)
-		}
-		partitionMap := resp.(map[string]interface{})
-		for key, value := range partitionMap {
-			topic := r_topic.FindStringSubmatch(key)[1]
-			partition := r_partition.FindStringSubmatch(key)[1]
-			if _, exists := d[topic]; !exists {
-				d[topic] = map[string]map[string]float64{}
-			}
-			d[topic][partition] = map[string]float64{}
-			//
-			v := value.(map[string]interface{})
-			d[topic][partition]["Size"] = v["Value"].(float64)
-		}
-	}
-	return d
-}
-
-func brokerMetrics(c *Config, brokerMeta kafkazk.BrokerMetaMap) (map[string]map[string]float64, error) {
-	query_template := "max:system.disk.free{host:%s}"
-	// Populate.
-	d := map[string]map[string]float64{}
-	for bid := range brokerMeta {
-		m := brokerMeta[bid]
-		query := fmt.Sprintf(query_template, m.Host)
-		start := time.Now().Add(-time.Duration(3600) * time.Second).Unix()
-		o, err := c.Client.QueryMetrics(start, time.Now().Unix(), query)
-		if err != nil {
-			return nil, err
-		}
-		broker_id := strconv.Itoa(bid)
-		if _, exists := d[broker_id]; !exists {
-			d[broker_id] = map[string]float64{}
-		}
-		d[broker_id]["StorageFree"] = *o[0].Points[0][1]
-	}
-	return d, nil
 }
 
 func main() {
